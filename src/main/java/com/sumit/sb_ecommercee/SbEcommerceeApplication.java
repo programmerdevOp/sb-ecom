ec2ce1d30f9516a10054403fd18b940aeaabcfa9
sumity29 <enc.sumitpal29@gmail.com>
2026-05-17 10:41:07 +0530

    pushing category model and controller code

---
+++ src/main/java/com/sumit/sb_ecommercee/SbEcommerceeApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.sb_ecommercee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcommerceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcommerceeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/sb_ecommercee/SbEcommerceeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.sb_ecommercee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcommerceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
