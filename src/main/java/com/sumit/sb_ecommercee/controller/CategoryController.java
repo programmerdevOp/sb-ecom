de3c4e754e72fb14da7b72ba5b509b205116b7fd
sumity29 <enc.sumitpal29@gmail.com>
2026-05-18 11:25:37 +0530

    adding request  mapping in  my controller

--- src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
+++ src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
@@ -23,19 +24,19 @@ public class CategoryController {
 
     //public List<Category> categories = new ArrayList<>();
 
-    @GetMapping("/api/public/categories")
+    @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> allCategories =  categoryService.getAllCategories();
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
-    @PostMapping("/api/public/categories")
+    @PostMapping("/public/categories")
     public ResponseEntity<String> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("New Category Added Successfully", HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
         try {
             String status = categoryService.deleteCategory(categoryId);
@@ -47,7 +48,7 @@ public class CategoryController {
 
     }
 
-    @PutMapping("/api/public/categories/{categoryId}")
+    @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category,
                                                  @PathVariable Long categoryId){
         try {
