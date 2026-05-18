8887b2120273992330adc1584a28adfac8ba2d5b
sumity29 <enc.sumitpal29@gmail.com>
2026-05-18 10:59:52 +0530

    fixing things  in update controller now  update controller working  fine

--- src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
+++ src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
@@ -47,7 +47,7 @@ public class CategoryController {
 
     }
 
-    @PutMapping("/api/admin/categories/{categoryId}")
+    @PutMapping("/api/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category,
                                                  @PathVariable Long categoryId){
         try {
