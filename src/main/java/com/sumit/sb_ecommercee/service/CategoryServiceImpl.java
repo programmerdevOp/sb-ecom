fe5f2d9777bcc743e69d39a21b12d2c1b83f0812
sumity29 <enc.sumitpal29@gmail.com>
2026-05-19 08:49:39 +0530

    optimising code

--- src/main/java/com/sumit/sb_ecommercee/service/CategoryServiceImpl.java
+++ src/main/java/com/sumit/sb_ecommercee/service/CategoryServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
-    private Long nextId = 1L;
+    //private Long nextId = 1L;
 
     private CategoryRepository categoryRepository;
 
@@ -28,25 +28,31 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+        //category.setCategoryId(nextId++);
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Category category = categories.stream()
-                .filter( c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        Category categoryToDelete = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        categoryRepository.delete(categoryToDelete);
+        return "Category with Id: " + categoryId + "founded successfully";
+//        List<Category> categories = categoryRepository.findAll();
+//        Category category = categories.stream()
+//                .filter( c -> c.getCategoryId().equals(categoryId))
+//                .findFirst()
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
 //        if(category == null) {
 //            return "Category is not found by this id";
 //        }
 
         //categories.remove(category);
-        categoryRepository.delete(category);
-        return "Category with Id: " + categoryId + "founded successfully";
+//        categoryRepository.delete(category);
+//        return "Category with Id: " + categoryId + "founded successfully";
     }
 
     @Override
@@ -63,18 +69,29 @@ public class CategoryServiceImpl implements CategoryService{
 //            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found");
 //        }
 
-        List<Category> categories =categoryRepository.findAll();
-        Optional<Category> categoryToUpdate = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
+//        List<Category> categories =categoryRepository.findAll();
+//        Optional<Category> categoryToUpdate = categories.stream()
+//                .filter(c -> c.getCategoryId().equals(categoryId))
+//                .findFirst();
+//
+//        if(categoryToUpdate.isPresent()){
+//            Category existingCategory =  categoryToUpdate.get();
+//            existingCategory.setCategoryName(category.getCategoryName());
+//            return existingCategory;
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+//        }
+        Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
 
-        if(categoryToUpdate.isPresent()){
-            Category existingCategory =  categoryToUpdate.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        Category savedCategory =  savedCategoryOptional.orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+
+        //savedCategory.setCategoryName(category.getCategoryName());
+        //categoryRepository.save(category);
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 
 
