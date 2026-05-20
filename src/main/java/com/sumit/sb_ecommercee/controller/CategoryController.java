c652cfae72ce6fb221d829605ba6f7a8848025aa
sumity29 <enc.sumitpal29@gmail.com>
2026-05-20 11:41:45 +0530

    cleaning code

--- src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
+++ src/main/java/com/sumit/sb_ecommercee/controller/CategoryController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -39,25 +37,14 @@ public class CategoryController {
 
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
             String status = categoryService.deleteCategory(categoryId);
-            //return new ResponseEntity<>(status, HttpStatus.OK);
             return ResponseEntity.ok(status);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
-
     }
 
     @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category,
                                                  @PathVariable Long categoryId){
-        try {
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("category with category Id: " + categoryId, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
-
     }
 }
--- src/main/java/com/sumit/sb_ecommercee/model/Category.java
+++ src/main/java/com/sumit/sb_ecommercee/model/Category.java
@@ -21,28 +21,4 @@ public class Category {
     @NotBlank
     private String categoryName;
 
-//    public Long getCategoryId() {
-//        return categoryId;
-//    }
-//
-//    public void setCategoryId(Long categoryId) {
-//        this.categoryId = categoryId;
-//    }
-//
-//    public String getCategoryName() {
-//        return categoryName;
-//    }
-//
-//    public void setCategoryName(String categoryName) {
-//        this.categoryName = categoryName;
-//    }
-//
-//    public Category(Long categoryId, String categoryName) {
-//        this.categoryId = categoryId;
-//        this.categoryName = categoryName;
-//    }
-//
-//    public Category(){
-//
-//    }
 }
